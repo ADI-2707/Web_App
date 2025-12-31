e8f306b06e8aa3ecb2c58c5912d0b5beba5e43f5
ADI-2707 <aditya21singh2707@gmail.com>
2025-12-31 14:36:01 +0530

    Hover effect added

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -4,11 +4,19 @@ import HeroSection from "../Components/HeroSection";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-center text-3xl md:text-6xl font-bold mt-6 hero-header">
-        Welcome to the Web App
-      </h1>
-      <div className="px-4 mt-20">
-        <HeroSection />
+
+        <h1 className="text-center text-3xl md:text-6xl font-bold mt-10 hero-header">
+          Welcome to the Web App
+        </h1>
+
+          <p className="text-center mt-8 md:text-lg px-4 hero-subheader">
+            Streamline your industrial operations with real-time recipe
+            management, advanced trend analytics, and comprehensive audit
+            compliance in one unified ecosystem.
+          </p>
+
+        <div className="mt-15">
+          <HeroSection />
       </div>
     </div>
   );
